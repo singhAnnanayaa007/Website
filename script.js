8f98f8d27194d10f145323049e4584d5c0014ab4
singhAnnanayaa007 <annanayaasingh@gmail.com>
2026-06-16 22:14:17 +0530

    Changes made

---
+++ script.js
@@ -0,0 +1,7 @@
+//THis is a comment
+console.log("This is my first website");
+//window.alert("This is an alert");
+document.getElementById("myh1").textContent = "Hello";
+document.getElementById("myp").textContent = "This is a paragraph";
+
+let age = 19;
